f2772ea92ba8d46ce511949e6d1c5965d5e43b6f
ErmiCaballero <ermicaballero@gmail.com>
2019-04-12 12:12:33 -0500

    Secind commit

--- WebPage/vinos_page/agregar/urls.py
+++ WebPage/vinos_page/agregar/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, HomeDetailView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name = 'home'), #La coma es imortante (Tupla)
+    path('agregar/<int:pk>/', HomeDetailView.as_view(), name = 'post_detail'),
 ]
